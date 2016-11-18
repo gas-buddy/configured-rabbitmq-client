f1c3a2af2faf62b60364fb4b6090cd026c4e09fc
Max Metral <max@pyralis.com>
2016-11-18 17:25:13 -0500

    Try again to fix wercker timing

--- tests/test_connection.js
+++ tests/test_connection.js
@@ -3,33 +3,37 @@ import net from 'net';
 import winston from 'winston';
 import RabbotClient from '../src/index';
 
-tap.test('wait for rabbit', async () => {
-  const s = new net.Socket();
-  for (let i = 0; i < 5; i += 1) {
+const mqConfig = {
+  host: process.env.RABBIT_HOST || 'rabbitmq',
+  port: process.env.RABBIT_PORT || 5672,
+  username: process.env.RABBIT_USER || 'guest',
+  password: process.env.RABBIT_PASSWORD || 'guest',
+};
+
+tap.test('wait for rabbit', async (t) => {
+  for (let i = 0; i < 10; i += 1) {
     let connected = false;
     await new Promise((accept) => {
-      s.once('error', accept);
-      s.connect({
-        host: process.env.RABBIT_HOST || 'rabbitmq',
-        port: process.env.RABBIT_PORT,
-      }, () => {
+      const s = new net.Socket();
+      s.once('error', () => {
+        t.ok(true, `Waiting for RabbitMQ response from ${mqConfig.host}:${mqConfig.port}`);
+        setTimeout(accept, 2500);
+      });
+      s.connect(mqConfig, () => {
         connected = true;
+        accept();
       });
     });
     if (connected) {
+      t.ok(true, 'RabbitMQ found');
       return;
     }
   }
+  t.fail('Could not connect to RabbitMQ');
 });
 
 tap.test('test_connection', async (t) => {
-  const config = {
-    hostname: process.env.RABBIT_HOST || 'rabbitmq',
-    port: process.env.RABBIT_PORT,
-    username: process.env.RABBIT_USER || 'guest',
-    password: process.env.RABBIT_PASSWORD || 'guest',
-  };
-  const mq = new RabbotClient(winston, config);
+  const mq = new RabbotClient(winston, mqConfig);
   const client = await mq.start();
   t.ok(client.publish, 'Should have a publish method');
   await mq.stop();
