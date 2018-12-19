915117f18f2584e79a123342ff8f5e154242ba35
Matthew Pavlinsky <mpavlinsky@gmail.com>
2018-12-19 15:23:42 -0500

    Route messages by queue and key to fix queues that share keys (#32)

--- tests/test_config.js
+++ tests/test_config.js
@@ -1,5 +1,6 @@
 import tap from 'tap';
 import net from 'net';
+import bluebird from 'bluebird';
 import RabbotClient from '../src/index';
 
 const mqConfig = {
@@ -7,19 +8,19 @@ const mqConfig = {
   port: process.env.RABBIT_PORT || 5672,
   username: process.env.RABBIT_USER || 'guest',
   password: process.env.RABBIT_PASSWORD || 'guest',
-  config: {
-    exchangeGroups: {
-      test: {
-        retries: 5,
-        retryDelay: 250,
-        keys: 'testkey',
-      },
-    },
-  },
   logging: {
     level: 2,
   },
 };
+
+function configWithExchangeGroups(exchangeGroups) {
+  return {
+    ...mqConfig,
+    config: {
+      exchangeGroups,
+    },
+  };
+}
 
 const ctx = { logger: console };
 
@@ -53,9 +54,15 @@ tap.test('wait for rabbit', async (t) => {
 });
 
 tap.test('test exchange group retry', async (t) => {
-  const retryCount = mqConfig.config.exchangeGroups.test.retries;
+  const retryCount = 5;
   t.plan((retryCount + 3) + retryCount);
-  const mq = new RabbotClient(ctx, mqConfig);
+  const mq = new RabbotClient(ctx, configWithExchangeGroups({
+    test: {
+      retries: retryCount,
+      retryDelay: 250,
+      keys: 'testkey',
+    },
+  }));
   await mq.start(ctx);
   let counter = 0;
   const errorMessage = 'retry again';
@@ -81,4 +88,40 @@ tap.test('test exchange group retry', async (t) => {
     t.equal(RabbotClient.activeMessages.size, 0, 'Should have 0 active message');
     await mq.stop(ctx);
   });
+});
+
+tap.test('test routing key reuse', async (t) => {
+  const mq = new RabbotClient(ctx, configWithExchangeGroups({
+    one: {
+      keys: 'number',
+    },
+    two: {
+      keys: 'number',
+    },
+  }));
+
+  await mq.start(ctx);
+  let receivedOne = 0;
+  let receivedTwo = 0;
+
+  await (async () => {
+    await mq.subscribe('one', 'number', async (context, message) => {
+      receivedOne += 1;
+      await message.ack();
+    });
+
+    await mq.subscribe('two', 'number', async (context, message) => {
+      receivedTwo += 1;
+      await message.ack();
+    });
+
+    await mq.publish('one', 'number', {});
+    await mq.publish('two', 'number', {});
+    await mq.publish('two', 'number', {});
+    await bluebird.delay(250);
+  })();
+
+  t.strictEqual(receivedOne, 1, 'Should have received exactly one message on the one handler.');
+  t.strictEqual(receivedTwo, 2, 'Should have received exactly two messages on the two handler.');
+  await mq.stop(ctx);
 });
